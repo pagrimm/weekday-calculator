5ae0eb0d41e68c2379a4714c7e554f292bb69c68
Peter Grimm <pagrimm@gmail.com>
2020-06-24 10:15:09 -0700

    added failing test for entering 31 days in months with 30
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- __tests__/weekday-calculator.tests.js
+++ __tests__/weekday-calculator.tests.js
@@ -5,4 +5,8 @@ describe('Weekday Calculator', () => {
   test('should be able to enter valid date', () => {
     expect(checkDate(2000, 1, 10)).toEqual(true);
   });
+
+  test('should not be able to enter 31 for months with 30 days', () => {
+    expect(checkDate(2000, 3, 31)).toEqual(false);
+  });
 });
--- src/weekday-calculator.js
+++ src/weekday-calculator.js
@@ -1,3 +1,6 @@
 export function checkDate (year, month, day) {
+  if (typeof year === "number" && month >= 0 && month <= 11 && day >= 0 && day <= 31) {
+    return true;
+  }
   return false;
 }
